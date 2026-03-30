b1ed65611a87ead6ef4c11966006c99704c98745
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-30 10:59:24 +0800

    fixed footer text

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='flex justify-center p-5'>
         <p>
-            Build by Joriel Brian Sudario
+            Built by Joriel Brian Sudario
         </p>
     </footer>
   )
