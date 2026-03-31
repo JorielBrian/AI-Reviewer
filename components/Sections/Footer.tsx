b059503ecbfb52e48002740f53fbfca37f811ebd
Joriel Brian Sudario <jorielsudario@gmail.com>
2026-03-31 18:30:24 +0800

    fixed the text design

--- components/Sections/Footer.tsx
+++ components/Sections/Footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='flex justify-center p-5'>
-        <p>
-            Built by Joriel Brian Sudario
-        </p>
+      <p className="text-gray-500 text-sm">
+          © {new Date().getFullYear()} Joriel Brian Sudario. All rights reserved.
+      </p>
     </footer>
   )
 }
